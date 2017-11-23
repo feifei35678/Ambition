e108ba2692dc0ba4817cdfba6989006cf0ff8883
Lisa Zhou <33638583+feifei35678@users.noreply.github.com>
2017-11-22 22:32:59 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function handleButtonClick() {
+	$('.welcome').append('Welcome');
+}
+
+function damnRight() {
+	var myWindow = window.open("", "Lisa said so", "width=350,height=100");
+	myWindow.document.write("<p>Damn right~ because I am THAT awesome!</p>");
+}
+
+function cantSayNo() {
+  confirm("YOU TAKE THAT BACK!!!!!!! TRY AGAIN!");
+}
